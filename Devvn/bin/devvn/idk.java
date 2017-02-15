0e3d92c86038afbe903289b2859b28e835f6b85d
Reisbrot <cornigruen@gmail.com>
2017-02-15 21:45:03 +0100

    Added Nothing
    Changed Nothing
    Removed Nothing

--- Devvn/bin/devvn/idk.java
+++ Devvn/bin/devvn/idk.java
@@ -13,17 +13,19 @@ public class idk {
 				int length = posMap.get(a).size();
 				for(int i = 0; i < length; i++){
 					if(!posMap.get(a).get(i).equals(""))x++;
-					else{System.out.println(x); xBB++; if(x >= 3){break;} else{xB = x+xBB; x = 0;}}
+					else{System.out.println(x); xBB++; if(x >= 3){break;} else{/*TODO finde richtigen wert um position von buchstaben richtig zu berechnen*/xB = x+xBB; x = 0;}}
+					System.out.println("zu kl. Felder + Feldtrenner: " + xB + " Feldtrenner: " + xBB);
 				}
 				for(int i = 0; i < list.size(); i++){
 					if(list.get(i).length() == x){
 				  
 						for(int y = 0; y < length; y++){
-							if(!posMap.get(a).get(y).isEmpty() && !posMap.get(a).get(y).equals(""))
-								if(Character.isAlphabetic(posMap.get(a).get(y).charAt(0)))
+	/*Wenn nicht leer*/		if(!posMap.get(a).get(y).isEmpty() && !posMap.get(a).get(y).equals(""))
+	/*Wenn schon bekannt*/		if(Character.isAlphabetic(posMap.get(a).get(y).charAt(0))){
+		System.out.println("y: "+y);
 									if(list.get(i).charAt(y - xB) == posMap.get(a).get(y).charAt(0)){
-										//System.out.println(list.get(i));
-									}
+										System.out.println(list.get(i));
+									}}
 						}
 					
 					}
@@ -31,6 +33,7 @@ public class idk {
 				}
 	
 				System.out.println("\n\n\n\n\n");
+				xBB = 0;
 				xB = 0;
 				x = 0;
 			
