7b5f555e01a1b3c1fe86068c6f698c4937c37c8d
Reisbrot <cornigruen@gmail.com>
2017-02-15 19:56:29 +0100

    Troubleshoooooting

--- Devvn/bin/devvn/idk.java
+++ Devvn/bin/devvn/idk.java
@@ -8,20 +8,21 @@ public class idk {
 	static int xB = 0;
 	static int xBB = 0;
 		public static void main(HashMap<Integer,List<String>> posMap, List<String> list){
-		//	for(int a = 0; a < posMap.size(); a++){
-				int length = posMap.get(0).size();
+			for(int a = 0; a < posMap.size(); a++){
+				System.out.println((a+1)+". Zeile");
+				int length = posMap.get(a).size();
 				for(int i = 0; i < length; i++){
-					if(!posMap.get(0).get(i).equals(""))x++;
+					if(!posMap.get(a).get(i).equals(""))x++;
 					else{System.out.println(x); xBB++; if(x >= 3){break;} else{xB = x+xBB; x = 0;}}
 				}
 				for(int i = 0; i < list.size(); i++){
 					if(list.get(i).length() == x){
 				  
 						for(int y = 0; y < length; y++){
-							if(!posMap.get(0).get(y).isEmpty() && !posMap.get(0).get(y).equals(""))
-								if(Character.isAlphabetic(posMap.get(0).get(y).charAt(0)))
-									if(list.get(i).charAt(y - xB) == posMap.get(0).get(y).charAt(0)){
-										System.out.println(list.get(i));
+							if(!posMap.get(a).get(y).isEmpty() && !posMap.get(a).get(y).equals(""))
+								if(Character.isAlphabetic(posMap.get(a).get(y).charAt(0)))
+									if(list.get(i).charAt(y - xB) == posMap.get(a).get(y).charAt(0)){
+										//System.out.println(list.get(i));
 									}
 						}
 					
@@ -29,9 +30,11 @@ public class idk {
 			
 				}
 	
-			//	System.out.println("\n\n\n\n\n");
+				System.out.println("\n\n\n\n\n");
+				xB = 0;
+				x = 0;
 			
-			//}
+			}
 			
 		}
 		
