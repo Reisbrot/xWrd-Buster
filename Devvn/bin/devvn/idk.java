151e294c36bd227dd77e4f28107adc396b8d2963
Reisbrot <cornigruen@gmail.com>
2017-02-19 21:55:53 +0100

    Restructured idk.java, not finished yet

--- Devvn/bin/devvn/idk.java
+++ Devvn/bin/devvn/idk.java
@@ -4,26 +4,53 @@ import java.util.HashMap;
 import java.util.List;
 
 public class idk {
-	static int x = 0;
-	static int xB = 0;
-	static int xBB = 0;
+	static int leer = 0;
+	static int zeichen = 0;
+	static int zeichen_folge = 0;
+	static int letterPos;
+	static int anfang = -1;
+	static int ende = 0;
+	static int zwei = 0;
+	static int zwende = 0;
 		public static void main(HashMap<Integer,List<String>> posMap, List<String> list){
 			for(int a = 0; a < posMap.size(); a++){
-				System.out.println((a+1)+". Zeile");
-				int length = posMap.get(a).size();
-				for(int i = 0; i < length; i++){
-					if(!posMap.get(a).get(i).equals(""))x++;
-					else{System.out.println(x); xBB++; if(x >= 3){break;} else{/*TODO finde richtigen wert um position von buchstaben richtig zu berechnen*/xB = x+xBB; x = 0;}}
-					System.out.println("zu kl. Felder + Feldtrenner: " + xB + " Feldtrenner: " + xBB);
-				}
+					System.out.println((a+1)+". Zeile");
+					int length = posMap.get(a).size();
+					
+					for(int i = 0; i < length; i++){
+						if(posMap.get(a).get(i).equals("")){
+								if(anfang != -1){
+									ende = length - i;
+								}
+							leer++;
+							zeichen_folge = 0;
+						}else
+							{
+								if(zeichen_folge >= 3){
+									if(anfang == -1)
+										anfang = leer + zeichen - 3;
+										else{ zwei = leer + zeichen - 3;
+										}
+									
+									zeichen_folge = 0;
+								}
+
+								zeichen++;
+								zeichen_folge++;
+								
+							}
+
+					}
+
+					
+					
 				for(int i = 0; i < list.size(); i++){
-					if(list.get(i).length() == x){
+					if(list.get(i).length() == (length - anfang - ende) /*?*/){
 				  
 						for(int y = 0; y < length; y++){
-	/*Wenn nicht leer*/		if(!posMap.get(a).get(y).isEmpty() && !posMap.get(a).get(y).equals(""))
-	/*Wenn schon bekannt*/		if(Character.isAlphabetic(posMap.get(a).get(y).charAt(0))){
-		System.out.println("y: "+y);
-									if(list.get(i).charAt(y - xB) == posMap.get(a).get(y).charAt(0)){
+							if(!posMap.get(a).get(y).equals(""))                                                           //!posMap.get(a).get(y).isEmpty() && 
+								if(Character.isAlphabetic(posMap.get(a).get(y).charAt(0))){
+									if(list.get(i).charAt(0 /*?*/) == posMap.get(a).get(y).charAt(0)){
 										System.out.println(list.get(i));
 									}}
 						}
@@ -33,9 +60,7 @@ public class idk {
 				}
 	
 				System.out.println("\n\n\n\n\n");
-				xBB = 0;
-				xB = 0;
-				x = 0;
+				leer = 0;
 			
 			}
 			
