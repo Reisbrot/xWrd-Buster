3bb12662a4e01887e72cc88104b7f04679571735
Reisbrot <cornigruen@gmail.com>
2017-02-22 21:27:54 +0100

    Dumb non necessarily commit without sense

--- Devvn/bin/devvn/idk.java
+++ Devvn/bin/devvn/idk.java
@@ -12,12 +12,16 @@ public class idk {
 	static int ende = 0;
 	static int zwei = 0;
 	static int zwende = 0;
+	static int posInline = 0;
+	static int finalPos = 0;
 		public static void main(HashMap<Integer,List<String>> posMap, List<String> list){
 			for(int a = 0; a < posMap.size(); a++){
 					System.out.println((a+1)+". Zeile");
 					int length = posMap.get(a).size();
 					
 					for(int i = 0; i < length; i++){
+						
+						
 						if(posMap.get(a).get(i).equals("")){
 								if(anfang != -1){
 									ende = length - i;
@@ -25,7 +29,12 @@ public class idk {
 							leer++;
 							zeichen_folge = 0;
 						}else
+							
+							
 							{
+								if(!Character.isDigit(posMap.get(a).get(i).charAt(0)))  //ka ob isAlphabetic oder isLetter oder was weiss ich richtig is also einfach so
+									{posInline = i; break;}
+									 
 								if(zeichen_folge >= 3){
 									if(anfang == -1)
 										anfang = leer + zeichen - 3;
@@ -37,23 +46,28 @@ public class idk {
 
 								zeichen++;
 								zeichen_folge++;
-								
 							}
+						
+						
 
 					}
-
+				    if(zwei == 0)finalPos = posInline - anfang; else{finalPos = posInline - zwei;}
 					
 					
 				for(int i = 0; i < list.size(); i++){
-					if(list.get(i).length() == (length - anfang - ende)){
-						System.out.println(list.get(i));
-					
+					if(list.get(i).length() == (finalPos)){
+						for(int x = 0; x < length; x++){
+							if(list.get(i).charAt(i) == posMap.get(a).get(x).charAt(0))
+							System.out.println(list.get(i));
+						}
 					}
 			
 				}
 	
 				System.out.println("\n\n\n\n\n");
 				leer = 0;
+				finalPos = 0;
+				posInline = 0;
 			
 			}
 			
