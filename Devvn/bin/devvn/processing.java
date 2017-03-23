42143a599371a0a5bde1e057cc4fc3ed5a03d5b3
Reisbrot <cornigruen@gmail.com>
2017-03-23 22:51:48 +0100

    Added possibleWords

--- Devvn/bin/devvn/processing.java
+++ Devvn/bin/devvn/processing.java
@@ -36,6 +36,7 @@ static boolean neu1 = true;
 static boolean neu2 = true;
 static boolean neu3 = true;
 static boolean ThatsTheWord;
+static int possibleWords;
 
 @SuppressWarnings("all")
 static void verarbeitung(List<String> field, ArrayList<String> known, char[] letters, int l, List<String> list, boolean dm) {
@@ -376,8 +377,7 @@ if(dm)				 System.out.println(relativeToWordBeginning + " in Wort " + (i+1) + " 
 			 }wl = 0; neu1 = true; neu2 = true; neu3 = true;
 			 } 
 		}
-		String word = wordBuilder1.toString();
-		compareToDict(word, list);
+		compareToDict(wordBuilder1, list);
 	    wordBuilder1.clear(); wordBuilder2.clear(); wordBuilder3.clear();
 		
 }
@@ -387,16 +387,18 @@ static void verCalc(){
 }
 
 @SuppressWarnings("all")
-static void compareToDict(String word, List<String> list){
+static void compareToDict(List<Character> word, List<String> list){
 	for(int i = 0; i < list.size(); i++){
 		ThatsTheWord = true;
-		if(!(list.get(i).length() == word.length())) ThatsTheWord = false;
-		for(int c = 0; c < word.length(); c++){
-			if(word.charAt(c) == ('#'));
-			else{if(!(word.charAt(c) == (list.get(i).charAt(c)))) ThatsTheWord = false;}
+		if(!(list.get(i).length() == word.size())) ThatsTheWord = false;
+		if(ThatsTheWord)
+		for(int c = 0; c < word.size(); c++){
+			if(word.get(c).equals('#'));
+			else{if(!(word.get(c) == (list.get(i).charAt(c)))) ThatsTheWord = false;}
 		}
-		if(ThatsTheWord) System.out.println("Das Wort kann ein " + list.get(i) + " sein.");
+		if(ThatsTheWord) {System.out.println("Das Wort kann ein " + list.get(i) + " sein."); possibleWords++;}
 	}
+	if(possibleWords == 1) //TODO Man nehme Zeile von field_part oder original rätsel und schaue nach den zahlen und gleiche dies ab mit wordlength und wordpositions für die buchstaben und ergänze sie
 }
 
 
