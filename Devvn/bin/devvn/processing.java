5a4a99921d95e417de57e1f479fea9613d6aa685
Reisbrot <cornigruen@gmail.com>
2017-03-25 21:15:02 +0100

    Tried to clean up processing();

--- Devvn/bin/devvn/processing.java
+++ Devvn/bin/devvn/processing.java
@@ -15,7 +15,7 @@ static HashMap<Integer,List<String>> hrzMap = new HashMap<Integer, List<String>>
 static HashMap<Integer,List<String>> verMap = new HashMap<Integer, List<String>>();
 static HashMap<Integer,String> startMap = new HashMap<Integer, String>();
 static String bufferLetter = "";
-static List<String> bufferList = new ArrayList<String>();
+static List<String> bufferPart = new ArrayList<String>();
 static int rows;
 static String number_docking = "";
 static String letter_docking = "";
@@ -40,57 +40,33 @@ static int possibleWords;
 static int c2d;
 static int PASSc2d;
 static char[] letters;
+static boolean init;
 
 @SuppressWarnings("all")
 static void verarbeitung(List<String> field, ArrayList<String> known, int letterl, int l, List<String> list, boolean dm) {
 	letters = new char[letterl];
 	
-	Arrays.fill(letters, 0, letters.length, ' ');
+if(!init) {Arrays.fill(letters, 0, letters.length, ' ');
 	
-	numbers = new int[known.size()];
-	cleaned_letters = new char[known.size()];
-    addKnownChars(known, dm);
+		  numbers = new int[known.size()];
+		  cleaned_letters = new char[known.size()];
+		  addKnownChars(known, dm);
    
-	for(int i=0; i < field.size()/l; i++){  //Die Schleife wird so oft durchlaufen wie´s Zeilen gibt
-			
-		    //Wieso ich kein Switch benutzt habe? Gab nen Error, das ist alles.
-		    if (i == 1){field_part1 = field.subList(0, l); fp1 = true;}
-            if (i == 1){field_part2 = field.subList(l, 2*l); fp2 = true;}
-            if (i == 2){field_part3 = field.subList(2*l, 3*l); fp3 = true;}
-            if (i == 3){field_part4 = field.subList(3*l, 4*l); fp4 = true;}
-            if (i == 4){field_part5 = field.subList(4*l, 5*l); fp5 = true;}
-            if (i == 5){field_part6 = field.subList(5*l, 6*l); fp6 = true;}
-            if (i == 6){field_part7 = field.subList(6*l, 7*l); fp7 = true;}
-            if (i == 7){field_part8 = field.subList(7*l, 8*l); fp8 = true;}
-            if (i == 8){field_part9 = field.subList(8*l, 9*l); fp9 = true;}
-            if (i == 9){field_part10 = field.subList(9*l, 10*l); fp10 = true;}
-            if (i == 10){field_part11 = field.subList(10*l, 11*l); fp11 = true;}
-            if (i == 11){field_part12 = field.subList(11*l, 12*l); fp12 = true;}
-            if (i == 12){field_part13 = field.subList(12*l, 13*l); fp13 = true;}
-            if (i == 13){field_part14 = field.subList(13*l, 14*l); fp14 = true;}
-            if (i == 14){field_part15 = field.subList(14*l, 15*l); fp15 = true;}
-            if (i == 15){field_part16 = field.subList(15*l, 16*l); fp16 = true;}
-            if (i == 16){field_part17 = field.subList(16*l, 17*l); fp17 = true;}
-            if (i == 17){field_part18 = field.subList(17*l, 18*l); fp18 = true;}
-            if (i == 18){field_part19 = field.subList(18*l, 19*l); fp19 = true;}
-            if (i == 19){field_part20 = field.subList(19*l, 20*l); fp20 = true;}
-            if (i == 20){field_part21 = field.subList(20*l, 21*l); fp21 = true;}
-            if (i == 21){field_part22 = field.subList(21*l, 22*l); fp22 = true;}
-            if (i >= 22){System.err.println("Index out of range - Rätsel zu lang, maximal 22 Zeilen!");}
-			
-	}
+		  field_init(field, l);}
+    
 	replacing_replacing(dm);
 	verticalice(dm);
 	
+if(!init){
 	for(int i = 0; i < hrzMap.size(); i++){
 		if(!hrzMap.get(i).equals("")) hrzMap_length++;
 	}
 	for(int i = 0; i < verMap.size(); i++){
 		if(!verMap.get(i).equals("")) verMap_length++;
 	}
-	
+}
     idk.main(hrzMap, list, dm, hrzMap_length);
-	
+	init = true;
 	//output.printProto(proto_output1, proto_output2, proto_output3, proto_output4, proto_output5, proto_output6, proto_output7, proto_output8, proto_output9, proto_output10, proto_output11, proto_output12, proto_output13, proto_output14, proto_output15, proto_output16, proto_output17, proto_output18, proto_output19, proto_output20, proto_output21, proto_output22, rows);			
     }
 
@@ -120,21 +96,22 @@ public static List<String> readFile(String fileName) {
 
 
 
-static List<String> replacing(boolean dm, Integer keyH, List<String> part, boolean fp){
+static List<String> replacing(boolean dm, Integer keyH, List<String> field_part, boolean fp){
 List<String> returned = new ArrayList<String>();
 if (fp){
-	for(int i = 0; i < part.size(); i++){
+	for(int i = 0; i < field_part.size(); i++){
 			for(int x = 0; x < letters.length; x++){
-				if (part.get(i).equals(""+(x+1))){if(!hrzMap.containsKey(keyH))if(letters[x]==' '){bufferLetter+=(part.get(i));}
-				                                                                     else{bufferLetter+=(Character.toString(letters[x]));} }
-
+				if(field_part.get(i).equals(""+(x+1))){
+				   if(letters[x]==' ')
+				   {bufferLetter+=(field_part.get(i));}
+				  else{bufferLetter+=(Character.toString(letters[x]));} }
 			}
-			bufferList.add(bufferLetter);
+			bufferPart.add(bufferLetter);
 			bufferLetter = "";
 
     }
-	returned.addAll(bufferList);
-	bufferList.clear();
+	returned.addAll(bufferPart);
+	bufferPart.clear();
 		return returned;
 
 
@@ -240,6 +217,7 @@ static void replacing_replacing(boolean dm){
 		keyH++;
 		
 		System.out.println(hrzMap);
+		keyH = 0;
 	}
 
 
@@ -429,5 +407,41 @@ if(dm)	System.out.println(chars);
 	addKnownChars(chars, dm);
 	}
 }
+
+
+
+static void field_init(List<String>field, int l){
+	for(int i=0; i < field.size()/l; i++){
+	    if (i == 1){field_part1 = field.subList(0, l); fp1 = true;}
+        if (i == 1){field_part2 = field.subList(l, 2*l); fp2 = true;}
+        if (i == 2){field_part3 = field.subList(2*l, 3*l); fp3 = true;}
+        if (i == 3){field_part4 = field.subList(3*l, 4*l); fp4 = true;}
+        if (i == 4){field_part5 = field.subList(4*l, 5*l); fp5 = true;}
+        if (i == 5){field_part6 = field.subList(5*l, 6*l); fp6 = true;}
+        if (i == 6){field_part7 = field.subList(6*l, 7*l); fp7 = true;}
+        if (i == 7){field_part8 = field.subList(7*l, 8*l); fp8 = true;}
+        if (i == 8){field_part9 = field.subList(8*l, 9*l); fp9 = true;}
+        if (i == 9){field_part10 = field.subList(9*l, 10*l); fp10 = true;}
+        if (i == 10){field_part11 = field.subList(10*l, 11*l); fp11 = true;}
+        if (i == 11){field_part12 = field.subList(11*l, 12*l); fp12 = true;}
+        if (i == 12){field_part13 = field.subList(12*l, 13*l); fp13 = true;}
+        if (i == 13){field_part14 = field.subList(13*l, 14*l); fp14 = true;}
+        if (i == 14){field_part15 = field.subList(14*l, 15*l); fp15 = true;}
+        if (i == 15){field_part16 = field.subList(15*l, 16*l); fp16 = true;}
+        if (i == 16){field_part17 = field.subList(16*l, 17*l); fp17 = true;}
+        if (i == 17){field_part18 = field.subList(17*l, 18*l); fp18 = true;}
+        if (i == 18){field_part19 = field.subList(18*l, 19*l); fp19 = true;}
+        if (i == 19){field_part20 = field.subList(19*l, 20*l); fp20 = true;}
+        if (i == 20){field_part21 = field.subList(20*l, 21*l); fp21 = true;}
+        if (i == 21){field_part22 = field.subList(21*l, 22*l); fp22 = true;}
+        if (i >= 22){System.err.println("Index out of range - Rätsel zu lang, maximal 22 Zeilen!");
+        }		
+}
+	
+}
+
+
+
+
 
 }
